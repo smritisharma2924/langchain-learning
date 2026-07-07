20702557959bfa73d472fd3bf40eee8605dab5b7
smritisharma2924 <smritisharma2905@gmail.com>
2026-07-08 00:19:00 +0530

    Implement first Gemini LLM demo

---
+++ langchain-models/1.LLMs/1_llm_demo.py
@@ -0,0 +1,10 @@
+# Gemini
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
+
+result = llm.invoke("What is the capital of India?")
+print(result.content)
