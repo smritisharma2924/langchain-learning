26cc5b505c5f65836d66336b1c3ad2deefb2ac84
smritisharma2924 <smritisharma2905@gmail.com>
2026-07-22 17:08:45 +0530

    Implement Embedding model for query

--- langchain-models/3.EmbeddedModels/5_embedding_gemini_query.py
+++ langchain-models/3.EmbeddedModels/5_embedding_gemini_query.py
@@ -1,24 +1,12 @@
-print("1")
-
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-print("2")
-
 from dotenv import load_dotenv
-print("3")
 
 load_dotenv()
-print("4")
 
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004"
+    model="models/gemini-embedding-2"
 )
-print("5")
 
 result = embeddings.embed_query("What is the capital of India?")
-print("6")
 
 print(result)
-
-# print("A")
-# import google.genai
-# print("B")
